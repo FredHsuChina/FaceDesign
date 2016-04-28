113259bfa69b78bd88490c32050888952a7a429d
FredHsu <FredHsu0822@hotmail.com>
2016-04-28 17:48:59 +0800

    Initial Commit

---
+++ DesighFace/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DesighFace
+//
+//  Created by 许文锋 on 16/4/28.
+//  Copyright © 2016年 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DesighFace/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DesighFace
+//
+//  Created by 许文锋 on 16/4/28.
+//  Copyright © 2016年 fred. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
